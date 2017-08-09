5aa8477725bd2c70bd11b6f411b66ed843bd553f
wanghui <wanghui@i5cnc.com>
2017-08-09 15:51:23 +0800

    add a calendar sample from web

---
+++ inter.js
@@ -0,0 +1,11 @@
+function inter() {
+    var inter = document.getElementById("inner")
+    if(inter.innerHTML === "") {
+           // alert("1")
+        inter.innerHTML="显示的文字"
+    } else {
+           // alert("2")
+
+        inter.innerHTML=""
+    }
+}
