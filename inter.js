8b12fe9407e4b4858bd2c26e205270291dfd5671
wanghui <wanghui@i5cnc.com>
2017-08-09 17:29:57 +0800

    update test

--- inter.js
+++ inter.js
@@ -1,11 +1,24 @@
-function inter() {
+// function inter() {
+//     var inter = document.getElementById("inner")
+//     if(inter.innerHTML === "") {
+//            // alert("1")
+//         inter.innerHTML="显示的文字"
+//     } else {
+//            // alert("2")
+//
+//         inter.innerHTML=""
+//     }
+// }
+
+// 如下写法外部无法直接引用inter()方法
+(function inter() {
     var inter = document.getElementById("inner")
     if(inter.innerHTML === "") {
-           // alert("1")
+        // alert("1")
         inter.innerHTML="显示的文字"
     } else {
-           // alert("2")
+        // alert("2")
 
         inter.innerHTML=""
     }
-}
+})();
